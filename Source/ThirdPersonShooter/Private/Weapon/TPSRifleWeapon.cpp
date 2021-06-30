91dfc7fa337ae987f1a377df383d29d07667d785
DESKTOP-7IAUGDM\Saskevich Aleksey <saskevich.aleksey@gmail.com>
2021-06-30 16:16:51 +0300

    VFX Build

--- Source/ThirdPersonShooter/Private/Weapon/TPSRifleWeapon.cpp
+++ Source/ThirdPersonShooter/Private/Weapon/TPSRifleWeapon.cpp
@@ -5,6 +5,7 @@
 #include "DrawDebugHelpers.h"
 #include "Weapon/Components/TPSWeaponFXComponent.h"
 #include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
 
 ATPSRifleWeapon::ATPSRifleWeapon()
 {
@@ -50,19 +51,14 @@ void ATPSRifleWeapon::MakeShot()
     FHitResult HitResult;
     MakeHit(HitResult, TraceStart, TraceEnd);
 
+    FVector TraceFXEnd = TraceEnd;
     if (HitResult.bBlockingHit)
     {
+        TraceFXEnd = HitResult.ImpactPoint;
         MakeDamage(HitResult);
-
-        // DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), HitResult.ImpactPoint, FColor::Red, false, 3.0f, 0, 3.0f);
-        // DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 10.0f, 24, FColor::Red, false, 5.0f);
         WeaponFXComponent->PlayImpactFX(HitResult);
     }
-    else
-    {
-        DrawDebugLine(GetWorld(), GetMuzzleWorldLocation(), TraceEnd, FColor::Red, false, 3.0f, 0, 3.0f);
-    }
-
+    SpawnTraceFX(GetMuzzleWorldLocation(), TraceFXEnd);
     DecreaseAmmo();
 }
 
@@ -101,5 +97,14 @@ void ATPSRifleWeapon::SetMuzzleFXVisibility(bool Visible)
     {
         MuzzleFXComponent->SetPaused(!Visible);
         MuzzleFXComponent->SetVisibility(Visible, true);
+    }
+}
+
+void ATPSRifleWeapon::SpawnTraceFX(const FVector& TraceStart, const FVector& TraceEnd)
+{
+    const auto TraceFXComponent = UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), TraceFX, TraceStart);
+    if (TraceFXComponent)
+    {
+        TraceFXComponent->SetNiagaraVariableVec3(TraceTargetName, TraceEnd);
     }
 }
--- Source/ThirdPersonShooter/Public/Weapon/TPSRifleWeapon.h
+++ Source/ThirdPersonShooter/Public/Weapon/TPSRifleWeapon.h
@@ -8,6 +8,7 @@
 
 class UTPSWeaponFXComponent;
 class UNiagaraComponent;
+class UNiagaraSystem;
 
 UCLASS()
 class THIRDPERSONSHOOTER_API ATPSRifleWeapon : public ATPSBaseWeapon
@@ -28,14 +29,20 @@ protected:
     UPROPERTY(VisibleAnywhere, Category = "VFX")
     UTPSWeaponFXComponent* WeaponFXComponent;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
     float TimeBetweenShots = 0.1f;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
     float BulletSpread = 1.5f;
 
-    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Weapon")
     float DamageAmount = 10.0f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+    UNiagaraSystem* TraceFX;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+    FString TraceTargetName = "TraceTarget";
 
 private:
     FTimerHandle ShotTimerHandle;
@@ -46,4 +53,5 @@ private:
     void MakeDamage(const FHitResult& HitResult);
     void InitMuzzleFX();
     void SetMuzzleFXVisibility(bool Visible);
+    void SpawnTraceFX(const FVector& TraceStart, const FVector& TraceEnd);
 };
