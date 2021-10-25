cb334828f7183c7a743864444b5822fe804af9d5
unknown <saskevich.aleksey@gmail.com>
2021-10-25 15:56:57 +0300

    DamageModifiers

--- Source/ThirdPersonShooter/Private/Components/TPSHealthComponent.cpp
+++ Source/ThirdPersonShooter/Private/Components/TPSHealthComponent.cpp
@@ -1,13 +1,13 @@
 // Third Person Shooter Game, All Rights Reserved.
 
 #include "Components/TPSHealthComponent.h"
-#include "GameFramework/Actor.h"
-#include "GameFramework/Pawn.h"
+#include "GameFramework/Character.h"
 #include "GameFramework/Controller.h"
 #include "Engine/World.h"
 #include "TimerManager.h"
 #include "Camera/CameraShakeBase.h"
 #include "TPSGameModeBase.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogHealthComponent, Display, Display);
 
@@ -28,11 +28,35 @@ void UTPSHealthComponent::BeginPlay()
     if (ComponentOwner)
     {
         ComponentOwner->OnTakeAnyDamage.AddDynamic(this, &UTPSHealthComponent::OnTakeAnyDamage);
+        ComponentOwner->OnTakePointDamage.AddDynamic(this, &UTPSHealthComponent::OnTakePointDamage);
+        ComponentOwner->OnTakeRadialDamage.AddDynamic(this, &UTPSHealthComponent::OnTakeRadialDamage);
     }
+}
+
+void UTPSHealthComponent::OnTakePointDamage(AActor* DamagedActor, float Damage, class AController* InstigatedBy, FVector HitLocation,
+    class UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection, const class UDamageType* DamageType,
+    AActor* DamageCauser)
+{
+    const auto FinalDamage = Damage * GetPointDamageModifier(DamagedActor, BoneName);
+
+    UE_LOG(LogHealthComponent, Display, TEXT("On point damage: %f, final damage: %f, bone: %s"), Damage, FinalDamage, *BoneName.ToString());
+    ApplyDamage(FinalDamage, InstigatedBy);
+}
+
+void UTPSHealthComponent::OnTakeRadialDamage(AActor* DamagedActor, float Damage, const class UDamageType* DamageType, FVector Origin,
+    FHitResult HitInfo, class AController* InstigatedBy, AActor* DamageCauser)
+{
+    UE_LOG(LogHealthComponent, Display, TEXT("On radial damage: %f"), Damage);
+    ApplyDamage(Damage, InstigatedBy);
 }
 
 void UTPSHealthComponent::OnTakeAnyDamage(
     AActor* DamagedActor, float Damage, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+    UE_LOG(LogHealthComponent, Display, TEXT("On any damage: %f"), Damage);
+}
+
+void UTPSHealthComponent::ApplyDamage(float Damage, AController* InstigatedBy)
 {
     if (Damage <= 0.0f || IsDead() || !GetWorld()) return;
 
@@ -109,4 +133,15 @@ void UTPSHealthComponent::Killed(AController* KillerController)
     const auto VictimController = Player ? Player->Controller : nullptr;
 
     GameMode->Killed(KillerController, VictimController);
+}
+
+float UTPSHealthComponent::GetPointDamageModifier(AActor* DamageActor, const FName& BoneName)
+{
+    const auto Character = Cast<ACharacter>(DamageActor);
+    if (!Character || !Character->GetMesh() || !Character->GetMesh()->GetBodyInstance(BoneName)) return 1.0f;
+
+    const auto PhysMaterial = Character->GetMesh()->GetBodyInstance(BoneName)->GetSimplePhysicalMaterial();
+    if (!DamageModifiers.Contains(PhysMaterial)) return 1.0f;
+
+    return DamageModifiers[PhysMaterial];
 }
